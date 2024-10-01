2537548389d42730ef09dee64a4cff37cdd20f56
AsdDDsa182 <chltmdgmlgo@naver.com>
2024-10-01 17:17:27 +0900

    Add files via upload

--- web/script.js
+++ web/script.js
@@ -1058,7 +1058,7 @@ function handleServicesAnimation() {
                     observer.unobserve(entry.target);
                 }
             });
-        }, { threshold: 0.2 });
+        }, { threshold: 0.5 });
 
         observer.observe(servicesSection);
     }
